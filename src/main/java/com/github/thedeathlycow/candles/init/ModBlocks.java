74d103e9723c1c7af3e4c95c474f3a0c09e43d84
TheDeathlyCow <thedeathlycow@gmail.com>
2020-08-01 18:27:00 -1000

    template crashes game :(

--- src/main/java/com/github/thedeathlycow/candles/init/ModBlocks.java
+++ src/main/java/com/github/thedeathlycow/candles/init/ModBlocks.java
@@ -15,29 +15,63 @@ public class ModBlocks {
 
     // func_235838_a_ gives off light!
 
-    public static final CandleBlock CANDLE_TEMPLATE = new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+    private static final CandleBlock CANDLE_TEMPLATE = new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
         return 3 + 3 * property.get(CandleBlock.CANDLES);
     }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME);
 
-    public static final Block BEESWAX_CANDLE = Setup.setup(CANDLE_TEMPLATE, "beeswax_candle");
+    public static final Block BEESWAX_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "beeswax_candle");
 
     // coloured candles
-    public static final Block WHITE_CANDLE = Setup.setup(CANDLE_TEMPLATE, "white_candle");
-    public static final Block ORANGE_CANDLE = Setup.setup(CANDLE_TEMPLATE, "orange_candle");
-    public static final Block MAGNETA_CANDLE = Setup.setup(CANDLE_TEMPLATE, "magenta_candle");
-    public static final Block LIGHT_BLUE_CANDLE = Setup.setup(CANDLE_TEMPLATE, "light_blue_candle");
-    public static final Block YELLOW_CANDLE = Setup.setup(CANDLE_TEMPLATE, "yellow_candle");
-    public static final Block LIME_CANDLE = Setup.setup(CANDLE_TEMPLATE, "lime_candle");
-    public static final Block PINK_CANDLE = Setup.setup(CANDLE_TEMPLATE, "pink_candle");
-    public static final Block GRAY_CANDLE = Setup.setup(CANDLE_TEMPLATE, "gray_candle");
-    public static final Block LIGHT_GRAY_CANDLE = Setup.setup(CANDLE_TEMPLATE, "light_gray_candle");
-    public static final Block CYAN_CANDLE = Setup.setup(CANDLE_TEMPLATE, "cyan_candle");
-    public static final Block PURPLE_CANDLE = Setup.setup(CANDLE_TEMPLATE, "purple_candle");
-    public static final Block BLUE_CANDLE = Setup.setup(CANDLE_TEMPLATE, "blue_candle");
-    public static final Block BROWN_CANDLE = Setup.setup(CANDLE_TEMPLATE, "brown_candle");
-    public static final Block GREEN_CANDLE = Setup.setup(CANDLE_TEMPLATE, "green_candle");
-    public static final Block RED_CANDLE = Setup.setup(CANDLE_TEMPLATE, "red_candle");
-    public static final Block BLACK_CANDLE = Setup.setup(CANDLE_TEMPLATE, "black_candle");
+    public static final Block WHITE_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "white_candle");
+    public static final Block ORANGE_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "orange_candle");
+    public static final Block MAGNETA_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "magenta_candle");
+    public static final Block LIGHT_BLUE_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "light_blue_candle");
+    public static final Block YELLOW_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "yellow_candle");
+    public static final Block LIME_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "lime_candle");
+    public static final Block PINK_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "pink_candle");
+    public static final Block GRAY_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "gray_candle");
+    public static final Block LIGHT_GRAY_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "light_gray_candle");
+    public static final Block CYAN_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "cyan_candle");
+    public static final Block PURPLE_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "purple_candle");
+    public static final Block BLUE_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "blue_candle");
+    public static final Block BROWN_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "brown_candle");
+    public static final Block GREEN_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "green_candle");
+    public static final Block RED_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "red_candle");
+    public static final Block BLACK_CANDLE = Setup.setup(new CandleBlock(Block.Properties.create(Material.MISCELLANEOUS, MaterialColor.ADOBE).func_235838_a_((property) -> {
+        return 3 + 3 * property.get(CandleBlock.CANDLES);
+    }).zeroHardnessAndResistance().sound(SoundType.field_235602_z_),  ParticleTypes.FLAME), "black_candle");
 
     public static final Block[] BLOCKS = {
             BEESWAX_CANDLE,
